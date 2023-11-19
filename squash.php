e617bc29d708d1e01197a5e763fe59b90493df9d
Lukasz <60584680+lukaszdev@users.noreply.github.com>
2023-11-19 21:21:45 +0100

    sprawdzam czy działa strona

---
+++ squash.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Squash</title>
+    <link rel="stylesheet" href="styl.scss">
+</head>
+<body>
+
+<div id="lewy">
+<nav class="nav">
+  <input id="menu" type="checkbox">
+  <label for="menu">Menu</label>
+  <ul class="menu">
+    <li>
+      <a href="#0">
+        <span>About</span>
+        <i class="fas fa-address-card" aria-hidden="true"></i>
+      </a>
+    </li>
+    <li>
+      <a href="#0">
+        <span>Projects</span>
+        <i class="fas fa-tasks" aria-hidden="true"></i>
+      </a>
+    </li>
+    <li>
+      <a href="#0">
+        <span>Clients</span>
+        <i class="fas fa-users" aria-hidden="true"></i>
+      </a>
+    </li>
+    <li>
+      <a href="#0">
+        <span>Contact</span>
+        <i class="fas fa-envelope-open-text" aria-hidden="true"></i>
+      </a>
+    </li>
+  </ul>
+</nav>
+
+
+
+</div>
+
+<div id="glowny">
+
+<div id="empty">
+test
+</div>
+
+<div id="pilka">
+<div class="bouncingball"></div>
+</div>
+
+</div>
+
+<div id="prawy">
+<div class="foo">
+  <span class="letter" data-letter="S"> </span>
+  <span class="letter" data-letter="Q"></span>
+  <span class="letter" data-letter="U"></span>
+  <span class="letter" data-letter="A"></span>
+  <span class="letter" data-letter="S"></span>
+  <span class="letter" data-letter="H"></span>
+
+</div>
+
+
+</div>
+     
+
+
+   
+
+
+
+</body>
+</html>
